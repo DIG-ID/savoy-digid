d49f7c3c7241cf5658f7714163ebc6bd3c168844
pedro-digid <pedro@dig.id>
2022-09-20 12:37:55 +0100

    laravel & npm

--- functions.php
+++ functions.php
@@ -1,11 +1,35 @@
 <?php
-	
-	/* Styles
-	=============================================================== */
-	
-	function nm_child_theme_styles() {
-        // Enqueue child theme styles
-        wp_enqueue_style( 'nm-child-theme', get_stylesheet_directory_uri() . '/style.css' );
-	}
-	add_action( 'wp_enqueue_scripts', 'nm_child_theme_styles', 1000 ); // Note: Use priority "1000" to include the stylesheet after the parent theme stylesheets
-    
+
+/* Styles
+=============================================================== */
+
+function nm_child_theme_styles() {
+	// Enqueue child theme styles
+
+	//Get the theme data
+	$the_theme     = wp_get_theme();
+	$theme_version = $the_theme->get( 'Version' );
+
+	// Register Theme main style.
+	wp_register_style( 'savoy-digid-styles', get_template_directory_uri() . '/dist/main.css', array(), $theme_version );
+
+	// Add styles inline.
+	//wp_add_inline_style( 'hotel-inside-theme-styles', hi_get_font_face_styles() );
+
+	// Enqueue theme stylesheet.
+	wp_enqueue_style( 'savoy-digid-styles' );
+
+	wp_register_script(
+		'savoy-digid-scripts',
+		get_template_directory_uri() . '/dist/main.js',
+		array(),
+		$theme_version,
+		true
+	);
+
+	wp_enqueue_script( 'savoy-digid-scripts' );
+
+	wp_enqueue_style( 'nm-child-theme', get_stylesheet_directory_uri() . '/style.css', array(), $theme_version );
+
+}
+add_action( 'wp_enqueue_scripts', 'nm_child_theme_styles', 1000 );// Note: Use priority "1000" to include the stylesheet after the parent theme stylesheets
---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+// webpack.mix.js
+
+let mix = require('laravel-mix');
+
+mix
+  .setPublicPath('dist')
+  .setResourceRoot('./')
+  .autoload({
+    jquery: ['$', 'window.jQuery']
+  })
+  .js('assets/js/main.js', 'dist')
+  .sass('assets/sass/main.sass', 'dist')
+  .sass('assets/sass/admin-login.sass', 'dist')
+
+  .disableNotifications()
+  .browserSync({
+    proxy: "localhost/stricker-shop",
+    files: ["./**/*.php", "./dist/*.js", "./dist/*.css"]
+  });
+
+if (!mix.inProduction()) {
+  mix
+    .webpackConfig({
+      devtool: "source-map"
+    })
+    .sourceMaps();
+}
+
+if (mix.inProduction()) {
+  mix.version();
+}
