03aa92a6f65ab4bd746ecb2e076397c6808f0c92
Daniel-Digid <daniel@dig.id>
2022-09-27 12:23:28 +0100

    Dev

--- template-parts/footer/bar-copyright.php
+++ template-parts/footer/bar-copyright.php
@@ -3,7 +3,14 @@
 	<div class="nm-footer-bar-inner">
 		<div class="nm-row">
 			<div class="col-md-6">
-				copyright here
+			<?php
+				$copyright_text = ( isset( $nm_theme_options['footer_bar_text'] ) && strlen( $nm_theme_options['footer_bar_text'] ) > 0 ) ? $nm_theme_options['footer_bar_text'] : '';
+				if ( $nm_theme_options['footer_bar_text_cr_year'] ) {
+					$copyright_text = sprintf( '&copy; %s %s', date( 'Y' ), $copyright_text );
+			} ?>
+			 <?php if ( ! $display_copyright_text ) : ?>
+				<p class="nm-menu-item-copyright menu-item"><span><?php echo wp_kses_post( $copyright_text ); ?></span></p>
+			<?php endif; ?>
 			</div>
 			<div class="col-md-6">
 				<?php
