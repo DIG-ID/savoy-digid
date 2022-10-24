e28aa4327a72272e2c4f242fc2857175e4361690
pedro-digid <pedro@dig.id>
2022-10-24 12:55:49 +0100

    up

--- functions.php
+++ functions.php
@@ -50,3 +50,12 @@ function digid_remove_checkout_fields( $fields ) {
 return $fields;
 
 }
+
+//add_action( 'woocommerce_payment_complete', 'my_change_status_function' );
+
+/*function my_change_status_function( $order_id ) {
+
+	$order = wc_get_order( $order_id );
+	$order->update_status( 'completed' );
+
+}
