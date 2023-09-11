fe8a5e7fdd317ff388eea23a35b8052044085025
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-09-11 10:55:50 +0100

    up

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -11,9 +11,9 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 7.0.1
- NM: Modified */
+ * @package WooCommerce\Templates
+ * @version 7.9.0
+ */
 
 defined( 'ABSPATH' ) || exit;
 
