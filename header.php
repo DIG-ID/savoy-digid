7b83c2a59cf2d64116a73be154af710c6c60d9c8
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-03-18 12:28:05 +0000

    added new GTM code

--- header.php
+++ header.php
@@ -11,17 +11,21 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="<?php echo esc_attr( implode ( ', ', $meta_viewport ) ); ?>">
-		<!-- Google tag (gtag.js) -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-204463361-1"></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-			gtag('config', 'UA-204463361-1');
-		</script>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?
+		id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TB4JNL4');</script>
+		<!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TB4JNL4"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 		<?php if ( $nm_theme_options['page_load_transition'] ) : ?>
 			<div id="nm-page-load-overlay" class="nm-page-load-overlay"></div>
 		<?php endif; ?>
